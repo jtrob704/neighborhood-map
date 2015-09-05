d482a66b3fdc5c653308001f418dfcdc20783074
Squeezeit <jaittariusrobinson@192.168.1.4>
2015-09-05 15:52:58 -0400

    Initial front end HTML & CSS. Added sample model data for 1 location

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var Model = {
+    locations: [
+        {
+            type: 'entertainment',
+            iconUrl: 'icons/entertainement.svg',
+            name: 'Discovery Place',
+            url: 'http://epicentrenc.com/',
+            lat: 35.225372,
+            long: -80.841967
+        }
+    ]
+}
