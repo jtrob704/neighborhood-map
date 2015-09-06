3b8e52f624db5d67bfcdef3575ce2bd4cd1363ef
Squeezeit <jaittariusrobinson@192.168.1.4>
2015-09-06 19:20:51 -0400

    Formatting updates and removed console command used for debugging

--- js/app.js
+++ js/app.js
@@ -18,24 +18,22 @@ var model = {
     currentPoint: ko.observable(null)
 };
 
-console.log(model.points[0].lat + "," + model.points[0].long);
-
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 35.2251901, lng: -80.8465473},
         zoom: 16
     });
-    
-var latLong = new google.maps.LatLng(model.points[0].lat + "," + model.points[0].long);
 
-        var marker = new google.maps.Marker({
-            map: map,
-            draggable: true,
-            animation: google.maps.Animation.DROP,
-            position: {lat: 35.225372, lng: -80.841967}
-        });
-marker.setMap(map);
+    var latLong = new google.maps.LatLng(model.points[0].lat + "," + model.points[0].long);
+
+    var marker = new google.maps.Marker({
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 35.225372, lng: -80.841967}
+    });
+    marker.setMap(map);
 }
 
 var viewModel = function () {
