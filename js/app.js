8be127c044e259332638057e2bb586bfca0dc591
Squeezeit <jaittariusrobinson@192.168.1.4>
2015-09-06 18:09:51 -0400

    Initial ViewModel. Rewrote app.js initMap function so that map displays properly

--- js/app.js
+++ js/app.js
@@ -18,69 +18,16 @@ var Model = {
     currentPoint: ko.observable(null)
 };
 
-var ViewModel = function () {
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 35.2251901, lng: -80.8465473},
+        zoom: 16
+    });
+}
 
-// Initialize Standard and Knockout Observable Variables
+var viewModel = function () {
+    var listObservable = ko.observableArray();
+}
 
-    var self = this;
-    self.dialogItem = ko.observable();
-    self.markerArray = ko.observableArray();
-    self.filterArray = ko.observableArray();
-    self.mapUnavailable = ko.observable(false);
-    self.listArray = ko.observableArray();
-    self.query = ko.observable("");
-    self.dialogVisible = ko.observable(false);
-    self.showMarkers = ko.observable(true);
-    var map, place;
-    //var weatherCity; 
-    var activityButton = false;
-
-// Initialize DOM using IIFE
-
-    var initDom = function() {
-
-// Verify Map Object in DOM and initialize map or display map error
-
-        if (typeof window.google === 'object' && typeof window.google.maps === 'object') {
-
-// Set initial map options
-
-            var mapOptions = {
-            // disableDefualtUI: true,
-                zoom: 16,
-                center: new google.maps.LatLng(35.2235249,-80.8406035),
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-
-// Instantiate new map object with call to Google Maps API
-
-            map = new google.maps.Map(document.getElementById('map'), mapOptions);
-                infowindow = new google.maps.InfoWindow({
-                content: null
-            });
-
-// Create an array containing points of interest using data Model for display on map
-
-            var pointsArray = Model.points;
-            var pointsArrayLength = pointsArray.length;
-
-            for (var x=0; x < pointsArrayLength; x++) {
-                var pointPosition = new google.maps.LatLng(
-                pointsArray[x].lat,
-                pointsArray[x].long
-                );
-
-                var marker = new google.maps.Marker({
-                    position: pointPosition,
-                    title: pointsArray[x].name,
-                    url: pointsArray[x].url,
-                    icon: pointsArray[x].iconUrl,
-                    map: map,
-                    type: pointsArray[x].type,
-                    highlight: pointsArray[x].highlight,                    
-                });            
-// Add marker to marker array to show google markers and to display array entries in location list
-
-                self.markerArray.push(marker);
-                self.listArray.push(marker);
-                
+ko.applyBindings(viewModel);
