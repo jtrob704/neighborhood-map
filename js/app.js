e496e59e68a8d77d7439070028b6555aa7c56aaf
Squeezeit <jaittariusrobinson@192.168.1.4>
2015-09-09 22:08:49 -0400

    Added model data for Discovery Place

--- js/app.js
+++ js/app.js
@@ -5,45 +5,53 @@
  */
 "use strict";
 
+//Add model data
 var locations = [{
+        type: 'entertainment',
+        name: 'EpiCentre',
+        url: 'http://epicentrenc.com/',
+        lat: 35.225372,
+        long: -80.841967
+    },{
+        type: 'education',
+        name: 'Discovery Place',
+        url: 'http://www.discoveryplace.org/',
+        lat: 35.2295793,
+        long: -80.8411225
+    }
         
-            type: 'entertainment',            
-            name: 'EpiCentre',
-            url: 'http://epicentrenc.com/',
-            lat: 35.225372,
-            long: -80.841967            
-            }
-    ];
+];
 
-
-
-
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 35.2251901, lng: -80.8465473},
         zoom: 16
     });
 
-    var contentString = "Dummy Content";
+//Create each location as an item
+    var items = function (data) {
+        var self = this;
+        self.type = ko.observable(data.type);
+        self.name = ko.observable(data.name);
+        self.url = ko.observable(data.url);
+        self.latLong = ko.observable(new google.maps.LatLng(data.lat, data.long));
 
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+//Create markers for each location
+        self.marker = new google.maps.Marker({
+            map: null,
+            animation: google.maps.Animation.DROP,
+            position: self.latLong(),
+            title: self.name()
+        });
 
-    var marker = new google.maps.Marker({
-        map: map,
-        animation: google.maps.Animation.DROP,
-        position: {lat: locations[0].lat, lng: locations[0].long}
-    });
-  marker.addListener('click', function() {
-      infowindow.open(map, marker);
-  });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+    }
+
+    var ViewModel = function () {
+            var self = this;
+            self.searchString = ko.observable('');
 }
 
-
-var viewModel = function () {
-    var listObservable = ko.observableArray();
-}
-
-ko.applyBindings(viewModel);
+ko.applyBindings(ViewModel);
