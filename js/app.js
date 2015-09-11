bb74aa947a622860ba9bdb3746fa29150822870a
Squeezeit <jaittariusrobinson@192.168.1.4>
2015-09-11 16:19:13 -0400

    Added InfoWindow for each location.

--- js/app.js
+++ js/app.js
@@ -19,32 +19,44 @@ var locations = [{
         lat: 35.2295793,
         long: -80.8411225
     }
-
 ];
-
-
 
 var ViewModel = function () {
     var self = this;
 
     var map;
-    
+
     function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 35.2251901, lng: -80.8465473},
-        zoom: 16
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 35.2251901, lng: -80.8465473},
+            zoom: 16
+        });
+    }
+    initMap();
+
+    var infowindow = new google.maps.InfoWindow({
+        maxWidth: 160
     });
-}
-initMap();
+
+    var markers = new Array();
 
     for (var i = 0, len = locations.length; i < len; i++) {
 
-        var latLong = new google.maps.LatLng(locations[i].lat, locations[i].long);
+        //var latLong = new google.maps.LatLng(locations[i].lat, locations[i].long);
 
         var marker = new google.maps.Marker({
-            position: latLong,
+            position: new google.maps.LatLng(locations[i].lat, locations[i].long),
             map: map
         });
+
+        markers.push(marker);
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                infowindow.setContent(locations[i].name);
+                infowindow.open(map, marker);
+            };
+        })(marker, i));
     }
 };
 
