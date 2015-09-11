aa4466f72235079a732c7e86d8f91177eb7570a8
Squeezeit <jaittariusrobinson@192.168.1.4>
2015-09-11 15:17:37 -0400

    Moved all global method inside of the ViewModel method

--- js/app.js
+++ js/app.js
@@ -12,36 +12,40 @@ var locations = [{
         url: 'http://epicentrenc.com/',
         lat: 35.225372,
         long: -80.841967
-    },{
+    }, {
         type: 'education',
         name: 'Discovery Place',
         url: 'http://www.discoveryplace.org/',
         lat: 35.2295793,
         long: -80.8411225
     }
-        
+
 ];
 
-var map = new google.maps.Map(document.getElementById('map'), {
+
+
+var ViewModel = function () {
+    var self = this;
+
+    var map;
+    
+    function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 35.2251901, lng: -80.8465473},
         zoom: 16
     });
-
-
-for(var i = 0, len = locations.length; i < len; i++) {
-    
-var latLong = new google.maps.LatLng(locations[i].lat,locations[i].long);
-
-var marker = new google.maps.Marker({
-    position: latLong,
-    map: map,
-    title: "Test"
-});
 }
+initMap();
 
-    var ViewModel = function () {
-            var self = this;
-            self.searchString = ko.observable('');
+    for (var i = 0, len = locations.length; i < len; i++) {
+
+        var latLong = new google.maps.LatLng(locations[i].lat, locations[i].long);
+
+        var marker = new google.maps.Marker({
+            position: latLong,
+            map: map
+        });
+    }
 };
 
 ko.applyBindings(ViewModel);
